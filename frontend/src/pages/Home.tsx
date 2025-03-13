9e265cd8f6fe464c4c923ae3c6714cb9afedc6d8
jp9141joao <jp9141joao@gmail.com>
2025-03-13 00:49:18 -0300

    fix: adjust on height page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,7 +9,8 @@ export const Home = () => {
     useEffect(() => {
         const setVh = () => {
             const vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
+            alert(vh)
+            document.documentElement.style.setProperty('--vh', `${vh - 0.2}px`);
         };
 
         setVh();
